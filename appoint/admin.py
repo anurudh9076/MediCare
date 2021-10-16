9540a50c11454ed8eda05ab2898a89e0c34c83bd
anurudh9076 <dzariozyaire@gmail.com>
2021-10-16 20:28:46 +0530

    cities added and doctors added

--- appoint/admin.py
+++ appoint/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import City,Speciality,Doctor,User,Slots,Appointment,Patient
 # Register your models here.
+
+#admin.site.register(User,UserAdmin)
+admin.site.register(City)
+admin.site.register(Speciality)
+admin.site.register(Doctor)
+admin.site.register(Slots)
+admin.site.register(Appointment)
+admin.site.register(Patient)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields =  ('first_name','last_name','username','email','password','is_doctor','is_patient')
+    list_display = ('first_name','username')
--- appoint/models.py
+++ appoint/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
-
-
 from .managers import CustomUserManager
 
 # Create your models here.
